ee647f57263e1e1d88c14894822f8cd6cc8072eb
Nikolay Beketov <nbek@miko.ru>
2016-05-10 18:23:17 +0400

    Update create_quickstart.sh

--- create_quickstart.sh
+++ create_quickstart.sh
@@ -4,7 +4,7 @@ ZATO_ENV=/opt/zato/env/qs-1
 ZATO_BIN=/opt/zato/current/bin/zato
 
 su - zato -c "rm -rf $ZATO_ENV && mkdir -p $ZATO_ENV"
-su - zato -c"$ZATO_BIN quickstart create $ZATO_ENV mysql localhost 6379 --kvdb_password '' --odb_host $ZATO_ODBHOST_ENV --odb_port 3306 --odb_user zatouser --odb_db_name zatodb --odb_password zatouser#1 --cluster_name mikocluster --servers 2 --verbose"
+su - zato -c"$ZATO_BIN quickstart create $ZATO_ENV mysql localhost 6379 --kvdb_password '' --odb_host $ZATO_ODBHOST_ENV --odb_port $ZATO_ODBPORT_ENV --odb_user $ZATO_ODBUSER_ENV --odb_db_name $ZATO_ODBDATABASE_ENV --odb_password $ZATO_ODBSECRET_ENV --cluster_name $ZATO_CLUSTERNAME --servers 2 $ZATO_ADDITIONALPARAM"
 su - zato -c "sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' $ZATO_ENV/load-balancer/config/repo/zato.config"
 su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server1/config/repo/server.conf"
 su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server2/config/repo/server.conf"
